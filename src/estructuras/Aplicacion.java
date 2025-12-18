806fdf795fad89bd663dab1605ba6d3a0a91a9c6
guillermogb <guillermogb@profe208b.informatica.cifpaviles.pa>
2025-12-18 10:02:16 +0100

    Excepciones.

--- src/estructuras/Aplicacion.java
+++ src/estructuras/Aplicacion.java
@@ -1,25 +1,48 @@
 package estructuras;
 
+import java.util.Scanner;
+
 public class Aplicacion {
-	
-	public Aplicacion() {
-		Cola unaCola=new Cola(3);
-		System.out.println(unaCola);
-		unaCola.encolar("A");
-		unaCola.encolar("B");
-		unaCola.encolar("C");
-		System.out.println(unaCola);
-		unaCola.encolar("D");
-		System.out.println(unaCola);
-		unaCola.encolar("E");
-		unaCola.encolar("F");
-		System.out.println(unaCola);
-		unaCola.encolar("G");
-		System.out.println(unaCola);
+
+	public Aplicacion() throws Exception {
+		this.menu();
 	}
 
-	public static void main(String[] args) {
-		Aplicacion a=new Aplicacion();
+	public static void menu() throws Exception {
+		Cola cola=new Cola(3);
+		Scanner s=new Scanner(System.in);
+		int opcion=0;
+		do {
+			System.out.println("Gestión de colas. Escoge una opción:");
+			System.out.println("1) Meter un String en la cola");
+			System.out.println("2) Sacar un String de la cola");
+			System.out.println("Otra opción: salir");
+			opcion=s.nextInt();
+			switch(opcion) {
+			case 1:
+				System.out.println("Introduce una cadena para encolar:");
+				String cadena=s.next();
+				cola.encolar(cadena); //Paso la pelota al método anterior (indicando throws Exception en su cabecera)
+				break;
+			case 2:
+				try {
+					System.out.println(cola.desencolar());
+				} catch (Exception e) {
+					//Dejo la pelota explotar:
+					//e.printStackTrace();
+					//Gestiono la pelota (me como el marrón):
+					System.out.println("La cola está vacía. Escoge otra opción.");
+				}
+				break;
+			default:
+				System.out.println("Saliendo de la aplicación.");	
+			}
+		} while(opcion==1||opcion==2);
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		Aplicacion a = new Aplicacion();
 	}
 
 }
--- src/estructuras/Cola.java
+++ src/estructuras/Cola.java
@@ -4,25 +4,55 @@ public class Cola {
 	
 	private String[] array;
 	private int tamanioOriginal;
+	private static boolean DEPURACION=true;
+	private final String[] malsonantes= {"CULO","CACA","PEDO","PIS"};
 
 	public Cola(int tamanioOriginal) {
 		this.tamanioOriginal=tamanioOriginal;
 		array=new String[tamanioOriginal];
 	}
 	
-	public void encolar(String s) { //Meter en la cola
-		if(!estaLlena())
+	public void encolar(String s) throws Exception { //Meter en la cola
+		if(esMalsonante(s))
+			throw new Exception("No se admiten palabras malsonantes.");
+		if(!estaLlena()) {
 			array[getFinal()]=s;
+			imprimirTraza("Cola con espacio disponible");
+			imprimirTraza("Se mete "+s+" en la cola.");
+			imprimirTraza("Estado de la cola: "+this.toString());
+		}
 		else {
+			imprimirTraza("Cola llena. Se amplía su tamaño.");
 			String[] arrayMayor=new String[array.length+tamanioOriginal];
 			clonar(array,arrayMayor);
 			array=arrayMayor;
+			imprimirTraza("Se mete "+s+" en la cola.");
 			array[getFinal()]=s;
+			imprimirTraza("Estado de la cola: "+this.toString());
 		}
 	}
 	
-	public String desencolar() { //Sacar de la cola
-		return "Se devuelve una cadena";
+	public String desencolar() throws Exception { // Sacar de la cola
+		if (array[0] == null) {
+			Exception e=new Exception("Se está intentando desencolar de una cola vacía.");
+			throw e;
+			//throw new Exception("Se está intentando..");
+		}
+		else {
+			imprimirTraza("Se desencola y se obtiene el elemento " + array[0]);
+			String cadenaRetorno = array[0];
+			imprimirTraza("Se comprime el array");
+			for (int i = 0; i < array.length - 1; i++)
+				array[i] = array[i + 1];
+			if (getFinal() < array.length - tamanioOriginal) {
+				imprimirTraza("Se libera espacio en memoria");
+				String[] arrayMenor = new String[array.length - tamanioOriginal];
+				arrayMenor = clonar(array, arrayMenor);
+				array = arrayMenor;
+			}
+			imprimirTraza("Estado de la cola " + this.toString());
+			return cadenaRetorno;
+		}
 	}
 	
 	public int getLongitud() {
@@ -55,9 +85,31 @@ public class Cola {
 	}
 	
 	private String[] clonar(String[] origen,String[] destino) {
+		imprimirTraza("Se clona origen (ID del objeto='"+origen+"') en destino (ID del objeto='"+destino+"'");
+		int menorTamanio;
+		if(origen.length<destino.length)
+			menorTamanio=origen.length;
+		else
+			menorTamanio=destino.length;
 		//Suponemos que el origen es más pequeño que el destino
-		for(int i=0;i<origen.length;i++)
+		for(int i=0;i<menorTamanio;i++)
 			destino[i]=origen[i];
 		return destino;
+	}
+	
+	private boolean esMalsonante(String palabra) {
+		boolean esMalsonante=false;
+		for(int i=0;i<this.malsonantes.length;i++)
+			if(palabra.toUpperCase().equals(malsonantes[i])) {
+				//No puedo usar == para comparar Strings.
+				esMalsonante=true;
+				break;
+			}
+		return esMalsonante;
+	}
+	
+	private static void imprimirTraza(String mensaje) {
+		if(DEPURACION==true)
+			System.out.println("[TRAZA] "+mensaje);
 	}
 }
